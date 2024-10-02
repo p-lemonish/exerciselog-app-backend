5bf4395c72f363f13f6d6b4c07162a26c7f6ae2e
Lemonish <patrikmihelson@gmail.com>
2024-10-02 03:13:13 +0300

    initialized project

---
+++ src/main/java/s24/backend/exerciselog/ExerciselogApplication.java
@@ -0,0 +1,13 @@
+package s24.backend.exerciselog;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ExerciselogApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ExerciselogApplication.class, args);
+	}
+
+}
---
+++ src/test/java/s24/backend/exerciselog/ExerciselogApplicationTests.java
@@ -0,0 +1,13 @@
+package s24.backend.exerciselog;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ExerciselogApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
