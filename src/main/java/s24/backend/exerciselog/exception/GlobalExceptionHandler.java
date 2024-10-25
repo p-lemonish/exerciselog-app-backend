926d85ee5de9192e0aa154b52a4264f974430f18
Lemonish <patrikmihelson@gmail.com>
2024-10-25 14:47:53 +0300

    Korjattu typo ja lisätty UsernameNotFoundException

--- src/main/java/s24/backend/exerciselog/exception/GlobalExceptionHandler.java
+++ src/main/java/s24/backend/exerciselog/exception/GlobalExceptionHandler.java
@@ -1,6 +1,7 @@
 package s24.backend.exerciselog.exception;
 
 import org.apache.coyote.BadRequestException;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
@@ -9,7 +10,13 @@ import org.springframework.web.bind.annotation.ExceptionHandler;
 public class GlobalExceptionHandler {
 
     @ExceptionHandler(ResourceNotFoundException.class)
-    public String handleResourseNotFound(ResourceNotFoundException ex, Model model) {
+    public String handleResourceNotFound(ResourceNotFoundException ex, Model model) {
+        model.addAttribute("errorMessage", ex.getMessage());
+        return "error/404";
+    }
+
+    @ExceptionHandler(UsernameNotFoundException.class)
+    public String handleUsernameNotFound(UsernameNotFoundException ex, Model model) {
         model.addAttribute("errorMessage", ex.getMessage());
         return "error/404";
     }
