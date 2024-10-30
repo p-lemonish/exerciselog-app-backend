37651a23b6e29f9ad3e7b2afd1f6c9b4afc3d5d0
Lemonish <patrikmihelson@gmail.com>
2024-10-30 22:19:24 +0200

    couple more fixes to cors and securityconfig

--- src/main/java/s24/backend/exerciselog/config/SecurityConfig.java
+++ src/main/java/s24/backend/exerciselog/config/SecurityConfig.java
@@ -5,6 +5,7 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.core.annotation.Order;
 import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.http.SessionCreationPolicy;
@@ -35,8 +36,9 @@ public class SecurityConfig {
     @Order(1)
     public SecurityFilterChain apiSecurityFilterChain(HttpSecurity http) throws Exception {
         http
-            .securityMatcher("/api/**") // apiSecurityChain will ONLY filter endpoints with /api/-beginning
+            .cors(Customizer.withDefaults())
             .csrf(csrf -> csrf.disable())
+            .securityMatcher("/api/**") // apiSecurityChain will ONLY filter endpoints with /api/-beginning
             .authorizeHttpRequests(auth -> auth
                 .requestMatchers("/api/login", "/api/register").permitAll()
                 .requestMatchers("/api/admin/**").hasRole("ADMIN")
