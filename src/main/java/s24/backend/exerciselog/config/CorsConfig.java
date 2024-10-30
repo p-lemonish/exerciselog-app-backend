5d58e8fd9eb6b539ab7bde86fbb8692c24419f4b
Lemonish <patrikmihelson@gmail.com>
2024-10-30 21:05:46 +0200

    added Vite 5173 port to CORS

--- src/main/java/s24/backend/exerciselog/config/CorsConfig.java
+++ src/main/java/s24/backend/exerciselog/config/CorsConfig.java
@@ -12,7 +12,7 @@ public class CorsConfig implements WebMvcConfigurer {
     @Override
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/api/**")
-                .allowedOrigins("http://localhost:3000") // React frontend origin
+                .allowedOrigins("http://localhost:5173", "http://localhost:3000") // React frontend origin (Vite)
                 .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
                 .allowedHeaders("*")
                 .allowCredentials(true);
