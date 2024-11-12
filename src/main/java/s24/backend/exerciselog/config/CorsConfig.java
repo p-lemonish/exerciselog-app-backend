15fb26057619b75c41fb1e00411a44eb70339eb4
Lemonish <patrikmihelson@gmail.com>
2024-11-12 14:17:40 +0200

    added a local ip to cors

--- src/main/java/s24/backend/exerciselog/config/CorsConfig.java
+++ src/main/java/s24/backend/exerciselog/config/CorsConfig.java
@@ -10,7 +10,7 @@ public class CorsConfig implements WebMvcConfigurer {
     @Override
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/api/**")
-                .allowedOrigins("http://localhost:5173", "http://localhost:3000", "http://frontend:80")
+                .allowedOrigins("http://localhost:5173", "http://localhost:3000", "http://frontend:80", "http://192.168.101.101:5173")
                 .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
                 .allowedHeaders("*")
                 .allowCredentials(true);
