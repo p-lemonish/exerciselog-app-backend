50772c6587da88a64da3b48a5cee81398202e84e
Lemonish <patrikmihelson@gmail.com>
2024-11-15 23:55:09 +0200

    Added database migration code for encrypting unencrypted data that persists in the database

--- src/main/java/s24/backend/exerciselog/ExerciselogApplication.java
+++ src/main/java/s24/backend/exerciselog/ExerciselogApplication.java
@@ -1,5 +1,8 @@
 package s24.backend.exerciselog;
 
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
@@ -7,8 +10,14 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.security.crypto.password.PasswordEncoder;
 
 import io.github.cdimascio.dotenv.Dotenv;
+import s24.backend.exerciselog.domain.entity.CompletedWorkout;
+import s24.backend.exerciselog.domain.entity.ExerciseLog;
+import s24.backend.exerciselog.domain.entity.PlannedExerciseLog;
 import s24.backend.exerciselog.domain.entity.Role;
+import s24.backend.exerciselog.domain.entity.User;
+import s24.backend.exerciselog.domain.entity.Workout;
 import s24.backend.exerciselog.repository.*;
+import s24.backend.exerciselog.util.SecurityUtils;
 
 @SpringBootApplication
 public class ExerciselogApplication {
@@ -76,4 +85,210 @@ public class ExerciselogApplication {
 				.orElseGet(() -> roleRepository.save(new Role("ADMIN")));
 		};
 	}
+
+	@Bean
+	public CommandLineRunner migrateData(
+		UserRepository userRepository,
+		PlannedExerciseLogRepository plannedExerciseLogRepository,
+		WorkoutRepository workoutRepository,
+		CompletedWorkoutRepository completedWorkoutRepository,
+		ExerciseLogRepository exerciseLogRepository,
+		@Value("${migrate.enabled:false}") boolean migrateEnabled) {
+		return (args) -> {
+			if(migrateEnabled) {
+				migrateUsers(userRepository);
+				migratePlannedExerciseLogs(plannedExerciseLogRepository);
+				migrateWorkouts(workoutRepository);
+				migrateCompletedWorkouts(completedWorkoutRepository);
+				migrateExerciseLogs(exerciseLogRepository);
+				System.out.println("Data migration completed");
+			} else {
+				System.out.println("Data migration is disabled");
+			}
+		};
+	}
+
+	private void migrateUsers(UserRepository userRepository) {
+		List<User> users = userRepository.findAll();
+		for (User user : users) {
+			boolean updated = false;
+
+			if (user.getUsername() != null) {
+				String username = user.getUsername();
+				
+				user.setUsername(username + " ");
+				System.out.println("Saved dummy username: " + user);
+				userRepository.save(user);
+
+				user.setUsername(username);
+				System.out.println("Saved real username: " + user);
+				userRepository.save(user);
+
+				updated = true;
+			}
+
+			if (user.getEmail() != null) {
+				String email = user.getEmail();
+				
+				user.setEmail(email + " ");
+				System.out.println("Saved dummy email: " + user);
+				userRepository.save(user);
+
+				user.setEmail(email);
+				System.out.println("Saved real email: " + user);
+				userRepository.save(user);
+
+				updated = true;
+			}
+
+			if (user.getUsernameHash() == null || user.getUsernameHash().isEmpty()) {
+				String username = user.getUsername();
+				if (username != null && !username.isEmpty()) {
+					String usernameHash = SecurityUtils.hash(username);
+					user.setUsernameHash(usernameHash);
+					System.out.println("Set usernameHash for User ID " + user.getId());
+					updated = true;
+				}
+			}
+
+			if (user.getEmailHash() == null || user.getEmailHash().isEmpty()) {
+				String email = user.getEmail();
+				if (email != null && !email.isEmpty()) {
+					String emailHash = SecurityUtils.hash(email);
+					user.setEmailHash(emailHash);
+					System.out.println("Set emailHash for User ID " + user.getId());
+					updated = true;
+				}
+			}
+
+			if (updated) {
+				System.out.println("Migrated User: " + user);
+				userRepository.save(user);
+			}
+		}
+	}
+
+
+	private void migratePlannedExerciseLogs(PlannedExerciseLogRepository plannedExerciseLogRepository) {
+		List<PlannedExerciseLog> plannedExerciseLogs = plannedExerciseLogRepository.findAll();
+		for (PlannedExerciseLog pel : plannedExerciseLogs) {
+			boolean updated = false;
+
+			if (pel.getNotes() != null) {
+				String notes = pel.getNotes();
+
+				pel.setNotes(notes + " ");
+				System.out.println("Saved dummy PlannedExerciseLog: " + pel);
+				plannedExerciseLogRepository.save(pel);
+
+				pel.setNotes(notes);
+				System.out.println("Saved real PlannedExerciseLog: " + pel);
+				plannedExerciseLogRepository.save(pel);
+
+				updated = true;
+			}
+
+			if (updated) {
+				System.out.println("Migrated PlannedExerciseLog: " + pel);
+			}
+		}
+	}
+
+
+	private void migrateWorkouts(WorkoutRepository workoutRepository) {
+		List<Workout> workouts = workoutRepository.findAll();
+		for (Workout workout : workouts) {
+			boolean updated = false;
+
+			if (workout.getName() != null) {
+				String name = workout.getName();
+
+				workout.setName(name + " ");
+				System.out.println("Saved dummy Workout: " + workout);
+				workoutRepository.save(workout);
+
+				workout.setName(name);
+				System.out.println("Saved real Workout: " + workout);
+				workoutRepository.save(workout);
+
+				updated = true;
+			}
+
+			if (workout.getNotes() != null) {
+				String notes = workout.getNotes();
+
+				workout.setNotes(notes + " ");
+				System.out.println("Saved dummy Workout Notes: " + workout);
+				workoutRepository.save(workout);
+
+				workout.setNotes(notes);
+				System.out.println("Saved real Workout Notes: " + workout);
+				workoutRepository.save(workout);
+
+				updated = true;
+			}
+
+			if (updated) {
+				System.out.println("Migrated Workout: " + workout);
+			}
+		}
+	}
+
+
+	private void migrateCompletedWorkouts(CompletedWorkoutRepository completedWorkoutRepository) {
+		List<CompletedWorkout> completedWorkouts = completedWorkoutRepository.findAll();
+		for (CompletedWorkout cw : completedWorkouts) {
+			boolean updated = false;
+
+			if (cw.getWorkoutName() != null) {
+				String name = cw.getWorkoutName();
+
+				cw.setWorkoutName(name + " ");
+				System.out.println("Saved dummy CompletedWorkout Name: " + cw);
+				completedWorkoutRepository.save(cw);
+
+				cw.setWorkoutName(name);
+				System.out.println("Saved real CompletedWorkout Name: " + cw);
+				completedWorkoutRepository.save(cw);
+
+				updated = true;
+			}
+
+			if (cw.getWorkoutNotes() != null) {
+				String notes = cw.getWorkoutNotes();
+
+				cw.setWorkoutNotes(notes + " ");
+				System.out.println("Saved dummy CompletedWorkout Notes: " + cw);
+				completedWorkoutRepository.save(cw);
+
+				cw.setWorkoutNotes(notes);
+				System.out.println("Saved real CompletedWorkout Notes: " + cw);
+				completedWorkoutRepository.save(cw);
+
+				updated = true;
+			}
+
+			if (updated) {
+				System.out.println("Migrated CompletedWorkout: " + cw);
+			}
+		}
+	}
+
+	private void migrateExerciseLogs(ExerciseLogRepository exerciseLogRepository) {
+		List<ExerciseLog> exerciseLogs = exerciseLogRepository.findAll();
+		for (ExerciseLog el : exerciseLogs) {
+			if (el.getNotes() != null) {
+				String notes = el.getNotes();
+
+				el.setNotes(notes + " ");
+				System.out.println("Saved dummy ExerciseLog: " + el);
+				exerciseLogRepository.save(el);
+
+				el.setNotes(notes);
+				System.out.println("Saved real ExerciseLog: " + el);
+				exerciseLogRepository.save(el);
+			}
+		}
+	}
+
 }
--- src/main/java/s24/backend/exerciselog/util/AttributeEncryptor.java
+++ src/main/java/s24/backend/exerciselog/util/AttributeEncryptor.java
@@ -2,6 +2,7 @@ package s24.backend.exerciselog.util;
 
 import java.security.GeneralSecurityException;
 
+import org.apache.commons.codec.binary.Base64;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 
@@ -15,6 +16,15 @@ public class AttributeEncryptor implements AttributeConverter<String, String> {
     @Autowired
     private EncryptionService encryptionService;
 
+    private boolean isBase64Encoded(String str) {
+        if (str == null || str.isEmpty()) {
+            return false;
+        }
+
+        byte[] byteArray = str.getBytes();
+        return Base64.isArrayByteBase64(byteArray);
+    }
+
     @Override
     public String convertToDatabaseColumn(String attribute) {
         try {
@@ -26,6 +36,12 @@ public class AttributeEncryptor implements AttributeConverter<String, String> {
 
     @Override
     public String convertToEntityAttribute(String dbData) {
+        if (dbData == null) return null;
+
+        if(!isBase64Encoded(dbData)) {
+            return dbData;
+        }
+
         try {
             return encryptionService.decrypt(dbData);
         } catch (GeneralSecurityException e) {
