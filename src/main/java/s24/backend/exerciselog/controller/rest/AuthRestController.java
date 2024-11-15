17c8e48758776c8f9d68c05a7d414442dcbd9afe
Lemonish <patrikmihelson@gmail.com>
2024-11-15 20:39:09 +0200

    Re-enabled registration. Edited /login-endpoint to use usernameHash to find the user looking to login

--- src/main/java/s24/backend/exerciselog/controller/rest/AuthRestController.java
+++ src/main/java/s24/backend/exerciselog/controller/rest/AuthRestController.java
@@ -8,17 +8,22 @@ import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.authentication.BadCredentialsException;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
 import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.*;
 
+import java.security.GeneralSecurityException;
 import java.util.*;
 
 import jakarta.validation.Valid;
 import s24.backend.exerciselog.domain.dto.*;
+import s24.backend.exerciselog.domain.entity.User;
 import s24.backend.exerciselog.exception.ResourceNotFoundException;
+import s24.backend.exerciselog.repository.UserRepository;
 import s24.backend.exerciselog.service.CustomUserDetailsService;
 import s24.backend.exerciselog.service.RegistrationService;
 import s24.backend.exerciselog.util.JwtUtil;
+import s24.backend.exerciselog.util.SecurityUtils;
 import s24.backend.exerciselog.util.ValidationUtil;
 
 @RestController
@@ -36,10 +41,12 @@ public class AuthRestController {
     @Autowired
     private RegistrationService registrationService;
 
-    @PostMapping("/api/register")
-    public ResponseEntity<?> registerUser(@Valid @RequestBody UserRegistrationDto userRegistrationDto, BindingResult result) throws BadRequestException, ResourceNotFoundException {
+    @Autowired
+    private UserRepository userRepository;
 
-        /*
+    @PostMapping("/api/register")
+    public ResponseEntity<?> registerUser(@Valid @RequestBody UserRegistrationDto userRegistrationDto, BindingResult result) throws BadRequestException, ResourceNotFoundException, GeneralSecurityException {
+
         // Check validation errors in RequestBody
         ResponseEntity<Map<String, String>> validationErrors = ValidationUtil.handleValidationErrors(result);
         if(validationErrors != null) {
@@ -49,22 +56,24 @@ public class AuthRestController {
         registrationService.registerUser(userRegistrationDto);
         
         return ResponseEntity.status(HttpStatus.CREATED).body("Registration OK!");
-        */
-
-        return ResponseEntity.status(HttpStatus.FORBIDDEN).body("Registration is currently disabled.");
     }
 
     @PostMapping("/api/login")
     public ResponseEntity<?> createAuthenticationToken(@RequestBody AuthenticationRequest authRequest) {
+        String username = authRequest.getUsername();
+        String usernameHash = SecurityUtils.hash(username);
+        String password = authRequest.getPassword();
+
+        final User user = userRepository.findByUsernameHash(usernameHash).orElseThrow(() -> new UsernameNotFoundException("User not found"));
+        UserDetails userDetails = customUserDetailsService.loadUserByUsername(username);
         try {
-            authenticationManager.authenticate(new UsernamePasswordAuthenticationToken(authRequest.getUsername(), authRequest.getPassword()));
+            authenticationManager.authenticate(new UsernamePasswordAuthenticationToken(userDetails.getUsername(), password));
         } catch(BadCredentialsException e) {
             return ResponseEntity.status(HttpStatus.UNAUTHORIZED)
                 .body("Incorrect username or password");
         }
-
-        final UserDetails userDetails = customUserDetailsService.loadUserByUsername(authRequest.getUsername());
-        final String jwt = jwtUtil.generateToken(userDetails);
+        final Long userId = user.getId();
+        final String jwt = jwtUtil.generateToken(userId);
         return ResponseEntity.ok(new AuthenticationResponse(jwt));
     }
     
