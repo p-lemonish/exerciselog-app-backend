e036d7e4d1810d6f46ec6382a0bb1ae37d115c22
Lemonish <patrikmihelson@gmail.com>
2025-03-21 20:29:28 +0200

    closes #6

--- src/main/java/s24/backend/exerciselog/service/WorkoutService.java
+++ src/main/java/s24/backend/exerciselog/service/WorkoutService.java
@@ -50,27 +50,34 @@ public class WorkoutService {
         List<Workout> workouts = workoutRepository.findByUser(user);
         return workoutMapper.toDtoList(workouts);
     }
+
     @Transactional
-    public WorkoutDto getWorkoutById(User user, Long id) {
+    public WorkoutDto getWorkoutById(User user, Long id) throws BadRequestException {
         Workout workout = workoutRepository.findById(id)
-            .orElseThrow(() -> new ResourceNotFoundException("Workout not found"));
+                .orElseThrow(() -> new ResourceNotFoundException("Workout not found"));
+        if (workout.getUser().getId() != user.getId()) {
+            throw new BadRequestException("You do not own this workout!");
+        }
         return workoutMapper.toDto(workout);
     }
+
     @Transactional
     public List<CompletedWorkoutDto> getUserCompletedWorkouts(User user) {
         List<CompletedWorkout> completedWorkouts = completedWorkoutRepository.findByUser(user);
         return completedWorkoutMapper.toDtoList(completedWorkouts);
     }
+
     @Transactional
     public List<PlannedExerciseLogDto> getUserPlannedExercises(User user) {
         List<PlannedExerciseLog> plannedExerciseLogs = plannedExerciseLogRepository.findByUser(user);
         return plannedExerciseLogMapper.toDtoList(plannedExerciseLogs);
     }
+
     @Transactional
     public void addWorkout(WorkoutDto workoutDto, User user) {
-
         List<Long> selectedExerciseIds = workoutDto.getSelectedExerciseIds();
-        List<PlannedExerciseLog> selectedPlannedExerciseLogs = plannedExerciseLogRepository.findAllById(selectedExerciseIds);
+        List<PlannedExerciseLog> selectedPlannedExerciseLogs = plannedExerciseLogRepository
+                .findAllById(selectedExerciseIds);
         Workout workout = workoutMapper.toEntity(workoutDto, user);
         workout.setPlannedExerciseLogs(selectedPlannedExerciseLogs);
         workoutRepository.save(workout);
@@ -78,41 +85,51 @@ public class WorkoutService {
 
     @Transactional
     public void editWorkout(WorkoutDto workoutDto, User user, Long id) throws BadRequestException {
-        if(workoutDto.getId() == null || !workoutDto.getId().equals(id)) {
+        if (workoutDto.getId() == null || !workoutDto.getId().equals(id)) {
             throw new BadRequestException("Workout ID and Request ID do not match");
         }
-        Workout workout = workoutRepository.findById(id).orElseThrow(() -> new ResourceNotFoundException("Workout not found"));
-        if(workoutDto.getPlannedDate() == null) {
+
+        Workout workout = workoutRepository.findById(id)
+                .orElseThrow(() -> new ResourceNotFoundException("Workout not found"));
+        if (workout.getUser().getId() != user.getId()) {
+            throw new BadRequestException("You do not own this workout!");
+        }
+
+        if (workoutDto.getPlannedDate() == null) {
             workoutDto.setPlannedDate(workout.getDate());
         }
 
         List<Long> selectedExerciseIds = workoutDto.getSelectedExerciseIds();
-        List<PlannedExerciseLog> selectedPlannedExerciseLogs = plannedExerciseLogRepository.findAllById(selectedExerciseIds);
+        List<PlannedExerciseLog> selectedPlannedExerciseLogs = plannedExerciseLogRepository
+                .findAllById(selectedExerciseIds);
         workout = workoutMapper.toEntity(workoutDto, user);
         workout.setId(id);
         workout.setPlannedExerciseLogs(selectedPlannedExerciseLogs);
         workoutRepository.save(workout);
     }
 
-    @Transactional 
+    @Transactional
     public void completeWorkout(Long workoutId, CompletedWorkoutDto completedWorkoutDto) throws BadRequestException {
         User currentUser = SecurityUtils.getCurrentUser();
         Workout workout = workoutRepository.findById(workoutId)
-            .orElseThrow(() -> new ResourceNotFoundException("Workout not found"));
-        
+                .orElseThrow(() -> new ResourceNotFoundException("Workout not found"));
+        if (workout.getUser().getId() != currentUser.getId()) {
+            throw new BadRequestException("You do not own this workout!");
+        }
+
         // Check if exerciseIds were changed in API request
         WorkoutDto workoutDto = workoutMapper.toDto(workout);
         List<Long> selectedExerciseIds = workoutDto.getSelectedExerciseIds();
 
         List<ExerciseLogDto> exerciseLogDtos = completedWorkoutDto.getExercises();
         List<Long> completedWorkoutDtoExerciseIds = exerciseLogDtos
-            .stream().map(ExerciseLogDto::getExerciseId)
-            .collect(Collectors.toList());
-        
-        if(!selectedExerciseIds.equals(completedWorkoutDtoExerciseIds)) {
+                .stream().map(ExerciseLogDto::getExerciseId)
+                .collect(Collectors.toList());
+
+        if (!selectedExerciseIds.equals(completedWorkoutDtoExerciseIds)) {
             throw new BadRequestException("SelectedExerciseIds and completedWorkoutDtoExerciseIds do not match");
         }
-        
+
         LocalDate now = LocalDate.now();
         CompletedWorkout completedWorkout = completedWorkoutMapper.toEntity(completedWorkoutDto);
         completedWorkout.setUser(currentUser);
@@ -124,25 +141,26 @@ public class WorkoutService {
         List<ExerciseLog> exerciseLogs = new ArrayList<>();
         for (ExerciseLogDto exerciseDto : completedWorkoutDto.getExercises()) {
             PlannedExerciseLog plannedExerciseLog = plannedExerciseLogRepository.findById(exerciseDto.getExerciseId())
-                .orElseThrow(() -> new ResourceNotFoundException("Planned Exercise not found"));
+                    .orElseThrow(() -> new ResourceNotFoundException("Planned Exercise not found"));
 
             // Check if setNumber in setLogDto was tampered with
-            if(exerciseDto.getSetLogDtoList() != null) {
+            if (exerciseDto.getSetLogDtoList() != null) {
                 int plannedSets = plannedExerciseLog.getPlannedSets();
-                List<Integer> expectedSetNumbers = IntStream.rangeClosed(1, plannedSets).boxed().collect(Collectors.toList());
-                List<Integer> givenSetNumbers = exerciseDto.getSetLogDtoList().stream().map(SetLogDto::getSetNumber).collect(Collectors.toList());
-                if(!expectedSetNumbers.equals(givenSetNumbers)) {
+                List<Integer> expectedSetNumbers = IntStream.rangeClosed(1, plannedSets).boxed()
+                        .collect(Collectors.toList());
+                List<Integer> givenSetNumbers = exerciseDto.getSetLogDtoList().stream().map(SetLogDto::getSetNumber)
+                        .collect(Collectors.toList());
+                if (!expectedSetNumbers.equals(givenSetNumbers)) {
                     throw new BadRequestException("Set number values are not as expected");
                 }
             }
-        
+
             ExerciseLog exerciseLog = exerciseLogMapper.toEntity(
-                exerciseDto,
-                new ExerciseLog(),
-                plannedExerciseLog,
-                currentUser,
-                workout
-            );
+                    exerciseDto,
+                    new ExerciseLog(),
+                    plannedExerciseLog,
+                    currentUser,
+                    workout);
             exerciseLog.setDate(now);
             exerciseLogs.add(exerciseLog);
             exerciseLogRepository.save(exerciseLog);
@@ -152,37 +170,53 @@ public class WorkoutService {
     }
 
     @Transactional
-    public CompletedWorkoutDto startWorkout(Long workoutId) {
-        Workout workout = workoutRepository.findById(workoutId).orElseThrow(() -> new ResourceNotFoundException("Workout not found"));
+    public CompletedWorkoutDto startWorkout(Long workoutId) throws BadRequestException {
+        User user = SecurityUtils.getCurrentUser();
+        Workout workout = workoutRepository.findById(workoutId)
+                .orElseThrow(() -> new ResourceNotFoundException("Workout not found"));
+        if (workout.getUser().getId() != user.getId()) {
+            throw new BadRequestException("You do not own this workout!");
+        }
 
         CompletedWorkoutDto completedWorkoutDto = new CompletedWorkoutDto();
         completedWorkoutDto.setWorkoutName(workout.getName());
         completedWorkoutDto.setPlannedDate(workout.getDate());
         completedWorkoutDto.setId(workoutId);
-        if(workout.getUser() != null) {
-            completedWorkoutDto.setUserId(workout.getUser().getId());
-        }
+        completedWorkoutDto.setUserId(workout.getUser().getId());
 
-        //Initialize exercises within workout
-        List<ExerciseLogDto> exercises = exerciseLogMapper.plannedExerciseLogListToDtoList(workout.getPlannedExerciseLogs());
+        // Initialize exercises within workout
+        List<ExerciseLogDto> exercises = exerciseLogMapper
+                .plannedExerciseLogListToDtoList(workout.getPlannedExerciseLogs());
         completedWorkoutDto.setExercises(exercises);
 
         return completedWorkoutDto;
     }
+
     @Transactional
-    public void deletePlannedWorkout(Long workoutId) {
-        Workout workout = workoutRepository.findById(workoutId).orElseThrow(() -> new ResourceNotFoundException("Workout not found"));
+    public void deletePlannedWorkout(Long workoutId) throws BadRequestException {
+        User user = SecurityUtils.getCurrentUser();
+        Workout workout = workoutRepository.findById(workoutId)
+                .orElseThrow(() -> new ResourceNotFoundException("Workout not found"));
+        if (workout.getUser().getId() != user.getId()) {
+            throw new BadRequestException("You do not own this workout!");
+        }
 
         // Set workout = null to avoid null references
         List<ExerciseLog> exerciseLogs = exerciseLogRepository.findByWorkout(workout);
-        for(ExerciseLog exerciseLog : exerciseLogs) {
+        for (ExerciseLog exerciseLog : exerciseLogs) {
             exerciseLog.setWorkout(null);
         }
         workoutRepository.delete(workout);
     }
+
     @Transactional
-    public void deleteCompletedWorkout(Long workoutId) {
-        CompletedWorkout completedWorkout = completedWorkoutRepository.findById(workoutId).orElseThrow(() -> new ResourceNotFoundException("Completed Workout not found"));
+    public void deleteCompletedWorkout(Long workoutId) throws BadRequestException {
+        User user = SecurityUtils.getCurrentUser();
+        CompletedWorkout completedWorkout = completedWorkoutRepository.findById(workoutId)
+                .orElseThrow(() -> new ResourceNotFoundException("Completed Workout not found"));
+        if (completedWorkout.getUser().getId() != user.getId()) {
+            throw new BadRequestException("You do not own this workout!");
+        }
         completedWorkoutRepository.delete(completedWorkout);
     }
 }
