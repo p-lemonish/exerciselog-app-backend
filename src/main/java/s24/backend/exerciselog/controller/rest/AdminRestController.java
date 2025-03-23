ba2cd704406a63f56a3f2dbe8c30a22bd456907c
Lemonish <patrikmihelson@gmail.com>
2025-03-23 20:29:51 +0200

    disabled admin api endpoints

--- src/main/java/s24/backend/exerciselog/controller/rest/AdminRestController.java
+++ src/main/java/s24/backend/exerciselog/controller/rest/AdminRestController.java
@@ -11,6 +11,7 @@ import org.springframework.web.bind.annotation.*;
 import s24.backend.exerciselog.domain.dto.RoleDto;
 import s24.backend.exerciselog.domain.dto.UserProfileDto;
 import s24.backend.exerciselog.domain.entity.User;
+import s24.backend.exerciselog.exception.ApiNotInUseException;
 import s24.backend.exerciselog.exception.ResourceNotFoundException;
 import s24.backend.exerciselog.mapper.UserMapper;
 import s24.backend.exerciselog.repository.UserRepository;
@@ -27,31 +28,45 @@ public class AdminRestController {
 
     @Autowired
     private UserMapper userMapper;
-    
+
     @GetMapping("/api/admin/users")
     public ResponseEntity<List<UserProfileDto>> getAllUsers() {
-        List<User> users = userRepository.findAll();
-        List<UserProfileDto> userProfileDtos = userMapper.toProfileDtoList(users);
-        return ResponseEntity.status(HttpStatus.OK).body(userProfileDtos);
+        throw new ApiNotInUseException("This API endpoint is not available");
+        /*
+         * List<User> users = userRepository.findAll();
+         * List<UserProfileDto> userProfileDtos = userMapper.toProfileDtoList(users);
+         * return ResponseEntity.status(HttpStatus.OK).body(userProfileDtos);
+         */
     }
 
     @GetMapping("/api/admin/users/{id}")
     public ResponseEntity<UserProfileDto> getUserById(@PathVariable Long id) {
-        User user = userRepository.findById(id).orElseThrow(() -> new ResourceNotFoundException("User not found"));
-        UserProfileDto userProfileDto = userMapper.toProfileDto(user);
-        return ResponseEntity.status(HttpStatus.OK).body(userProfileDto);
+        throw new ApiNotInUseException("This API endpoint is not available");
+        /*
+         * User user = userRepository.findById(id).orElseThrow(() -> new
+         * ResourceNotFoundException("User not found"));
+         * UserProfileDto userProfileDto = userMapper.toProfileDto(user);
+         * return ResponseEntity.status(HttpStatus.OK).body(userProfileDto);
+         */
     }
 
     @PutMapping("/api/admin/users/{id}")
-    public ResponseEntity<?> editUserRole(@PathVariable Long id, @RequestBody RoleDto roleDto) throws ResourceNotFoundException, BadRequestException {
-        adminService.updateUserRole(id, roleDto);
-        return ResponseEntity.status(HttpStatus.OK).body("User role updated");
+    public ResponseEntity<?> editUserRole(@PathVariable Long id, @RequestBody RoleDto roleDto)
+            throws ResourceNotFoundException, BadRequestException {
+        throw new ApiNotInUseException("This API endpoint is not available");
+        /*
+         * adminService.updateUserRole(id, roleDto);
+         * return ResponseEntity.status(HttpStatus.OK).body("User role updated");
+         */
     }
 
     @DeleteMapping("/api/admin/users/{id}")
     public ResponseEntity<?> deleteUser(@PathVariable Long id) throws BadRequestException {
-        adminService.deleteUser(id);
-        return ResponseEntity.status(HttpStatus.NO_CONTENT).build();
+        throw new ApiNotInUseException("This API endpoint is not available");
+        /*
+         * adminService.deleteUser(id);
+         * return ResponseEntity.status(HttpStatus.NO_CONTENT).build();
+         */
     }
-    
+
 }
